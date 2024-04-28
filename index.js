99e1203b624440f6903d7bf8a7ee04baba8001fc
chistev <stephenowabie@gmail.com>
2024-04-28 21:47:44 +0100

    designed the sign up page and moved css styles to a styles.css

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000; // Choose a port for your server
 
+// Use express.static middleware to serve static files
+app.use(express.static('public'));
+
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
 
